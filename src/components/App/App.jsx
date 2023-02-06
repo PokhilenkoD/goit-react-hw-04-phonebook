8e6da5fb8483a66ac4cb36d795a718519c62673a
PokhilenkoD <pokhilenko.d@gmail.com>
2023-02-06 12:30:48 +0100

    fixes bags v1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,24 +6,24 @@ import { AppBox, MainTitle, SubTitle } from './App.styled';
 import { useEffect, useState } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const storageArray = JSON.parse(localStorage.getItem('contacts'));
+    if (storageArray) {
+      return [...storageArray];
+    } else {
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+    }
+  });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  useEffect(() => {
-    const storageArray = JSON.parse(localStorage.getItem('contacts'));
-    if (storageArray) {
-      setContacts([...storageArray]);
-    }
-  }, []);
 
   const addsNameContacts = value => {
     const verifyContact = contacts.some(contact => {
